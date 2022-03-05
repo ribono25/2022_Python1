b7001c8ee8fa2975b507bd1da753dbb36badcfa0
SeongHyeon Kim <94503162+ribono25@users.noreply.github.com>
2022-03-05 22:07:10 +0900

    Delete 6_흐름과 제어_If directory

--- 6_흐름과 제어_If/6-1.py
+++
@@ -1,6 +0,0 @@
-num = int(input())
-
-for i in range(1, num+1):
-    if num % i == 0:
-        print("%d(은)는 %d의 약수입니다." % (i, num))
-
--- 6_흐름과 제어_If/6-8.py
+++
@@ -1,9 +0,0 @@
-for i in range(200, 300, 2):
-    first = i // 100
-    second = i // 10 - (i // 100) * 10
-    third = i % 10
-
-    if first%2==0 and second%2==0 and third%2==0:
-        if i == 288: print(i)
-        else:
-            print(i, end=",")
